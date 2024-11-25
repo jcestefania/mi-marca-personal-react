26353bf137c72f54076a14b05eb45be244bce4ad
juancaaaa <juan.estefania@cunef.edu>
2024-11-25 14:36:40 +0100

    Refactor project structure by removing old components and styles, and adding new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
+import Header from './components/Header'; // Ruta relativa correcta
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import ContactForm from './components/ContactForm';
