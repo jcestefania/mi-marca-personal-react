82a76f6a1c6e360651d72fcfd2218202a9d0f63c
juancaaaa <juan.estefania@cunef.edu>
2024-11-25 19:58:12 +0100

    Update Header and Footer styles; change background color to black, add social media links with hover effects, and enhance layout for better presentation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './styles/Footer.css';
+import React from "react";
+import "./styles/Footer.css";
+import linkedinLogo from "../assets/img/linkedin_white.jpg";
+import githubLogo from "../assets/img/github_white.jpg";
 
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Juan Carlos Estefanía Ovejero. Todos los derechos reservados.</p>
+      <div className="social-links">
+        {/* Enlace a LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/juan-carlos-estefan%C3%ADa-ovejero-b4b8862b3/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinLogo} alt="LinkedIn" className="social-icon" />
+        </a>
+
+        {/* Enlace a GitHub */}
+        <a
+          href="https://github.com/jcestefania"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubLogo} alt="GitHub" className="social-icon" />
+        </a>
+      </div>
+
+      <p>© 2024 Juan Carlos Estefanía Ovejero. Todos los derechos reservados.</p>
     </footer>
   );
 }
 
 export default Footer;
+
