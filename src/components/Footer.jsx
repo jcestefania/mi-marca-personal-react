ffd36f2715aedc64a8b5e494e6ed37e7466fe19d
juancaaaa <juan.estefania@cunef.edu>
2024-11-29 19:11:25 +0100

    Update Footer.jsx and Footer.css: restructure footer layout, enhance social links section, and improve styles for better user interaction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,37 @@ import githubLogo from "../assets/img/github_white.jpg";
 function Footer() {
   return (
     <footer>
-      <div className="social-links">
-        {/* Enlace a LinkedIn */}
-        <a
-          href="https://www.linkedin.com/in/juan-carlos-estefan%C3%ADa-ovejero-b4b8862b3/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={linkedinLogo} alt="LinkedIn" className="social-icon" />
-        </a>
+      <ul>
+        <li><a href="#sobre-mi">Sobre mí</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+      </ul>
 
-        {/* Enlace a GitHub */}
-        <a
-          href="https://github.com/jcestefania"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={githubLogo} alt="GitHub" className="social-icon" />
-        </a>
+      <div className="social-section">
+        <h3>Redes Sociales</h3>
+        <p>Contacta conmigo a través de las redes sociales.</p>
+        <div className="social-links">
+          {/* Enlace a LinkedIn */}
+          <a
+            href="https://www.linkedin.com/in/juan-carlos-estefan%C3%ADa-ovejero-b4b8862b3/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="LinkedIn" className="social-icon" />
+          </a>
+
+          {/* Enlace a GitHub */}
+          <a
+            href="https://github.com/jcestefania"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubLogo} alt="GitHub" className="social-icon" />
+          </a>
+        </div>
       </div>
 
-      <p>© 2024 Juan Carlos Estefanía Ovejero. Todos los derechos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Juan Carlos Estefanía Ovejero. Todos los derechos reservados.</p>
     </footer>
   );
 }
